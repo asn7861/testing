34a0fb603822141dcb2985cc587662de2a2f8057
asn7861 <anas.internativelabs@gmail.com>
2023-11-13 09:21:19 -0800

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "success" });
+});
+
+app.post("/add", (req, res) => {
+
+    let { email, password } = req.body;
+    let data = {
+        email,
+        password
+    }
+    let token = jwt.sign(data, "12345678");
+    return res.status(200).json({ token })
+})
+
+app.get("/getusers", (req, res) => {
+    let authorization = req.headers['authorization'].split(' ')[1];
+    console.log(authorization);
+    let mytoken = jwt.verify(authorization, "12345678");
+    return res.status(200).json({ mytoken });
+})
+
+app.listen(4000, () => {
+    console.log("server is running at port 4000");
+});
+
